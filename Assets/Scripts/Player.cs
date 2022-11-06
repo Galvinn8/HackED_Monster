3ac01dbc57d589089a39e40d4feb9a6f87bce22f
Galvinn8 <zongxin@ualberta.ca>
2022-11-05 19:34:50 -0600

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]  // still keep the variable private, but show them in the terminal
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    // only declare them , hasn't give them a reference yet
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private Animator anim;
+
+    private string WALK_ANIMATION = "Walk";
+
+    private bool isGrounded = true;
+    private string G
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false; // can only jump once
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);  // push the object right up
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    // FixedUpdate is not called every frame like the update function
+    void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce; // multiply with time deltaTime to make the movement smooth
+
+    }
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            // object going to the left side
+            anim.SetBool(WALK_ANIMATION, true);  // WALK_ANIMATION is the reference to the Walk parameter int eh transition
+            sr.flipX = true;  //flip it under the component SpriteRenderer
+
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false  );
+        }
+
+
+    }
+}
